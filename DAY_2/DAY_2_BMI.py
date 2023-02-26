f8a6efbbc5769020650239dabb5c21f57f663443
Vishwaa-MS <93870138+Vishwaa-MS@users.noreply.github.com>
2023-02-26 18:13:00 +0530

    Add files via upload

---
+++ DAY_2/DAY_2_BMI.py
@@ -0,0 +1,7 @@
+#calculation of BMI
+height=(input('Enter Your Height: '))
+weight=(input('Enter Your Weight: '))
+bmi=int(weight)//float(height)**2
+print('YOUR BMI IS:',int(bmi))
+             
+              
