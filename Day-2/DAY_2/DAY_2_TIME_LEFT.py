86b0c9f3eeaeadeb83f97f91b1327da390fe1dc0
Vishwaa-MS <93870138+Vishwaa-MS@users.noreply.github.com>
2023-02-26 18:12:21 +0530

    Delete Day-2/DAY_2 directory

--- Day-2/DAY_2/DAY_2_TIME_LEFT.py
+++
@@ -1,7 +0,0 @@
-#Remaining time 
-age=int(input('Enter Your Age: '))
-years_remaining=19-age
-no_of_days_remaining=years_remaining*365
-no_of_weeks_remaining=years_remaining*52
-no_of_months_remaining=years_remaining*12
-print(f'You have {no_of_days_remaining} days,{no_of_weeks_remaining} weeks,and {no_of_months_remaining} months left')
