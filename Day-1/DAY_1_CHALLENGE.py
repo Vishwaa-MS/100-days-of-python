1aed7b71ba91df2cfee51d2bd734e0ae81beb7ec
Vishwaa-MS <93870138+Vishwaa-MS@users.noreply.github.com>
2023-02-26 18:07:41 +0530

    Add files via upload

---
+++ Day-1/DAY_1_CHALLENGE.py
@@ -0,0 +1,10 @@
+#DAY-1
+print('welcome to Team name generator !')
+city=input("what's the name of the city you born? : ")
+pet=input("what's your favourite animal name? : ")
+combined=city+' '+pet
+print('\nYour Team name could be: '+combined)
+
+
+
+
